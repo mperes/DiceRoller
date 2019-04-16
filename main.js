024d27c66633831e63ce0d8c201282ffdf4f2a21
Miguel Peres <m.peres@gmail.com>
2019-04-16 09:07:23 -0300

    adding ready class on load

--- main.js
+++ main.js
@@ -176,6 +176,7 @@ function preLoadAndStart() {
     var tmpImg = new Image() ;
     tmpImg.src = 'img/fate_deck.jpg';
     tmpImg.onload = function() {
+      $('body').addClass('ready');
       gameSession = new GameSession(false, 6);
     };
   });
